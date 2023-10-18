dbe1310ea285191b70087e52b7ff1f084da90bda
Illia <ilyusha.drozd.2016@gmail.com>
2023-10-18 13:02:04 +0300

    init 2 lab

--- 2/main.py
+++ 2/main.py
@@ -1,119 +1,113 @@
-from NeuroNetwork import NeuroNetwork
 import numpy as np
-# Вхідні дані
-initial_layer = [
-  np.array([
-        [0, 1, 1, 1, 0],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [0, 1, 1, 1, 0]
-    ]),
-    np.array([
-        [0, 0, 1, 0, 0],
-        [0, 1, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 1, 1, 1, 0]
-    ]),
-    np.array([
-        [0, 1, 1, 1, 0],
-        [1, 0, 0, 0, 1],
-        [0, 0, 0, 0, 1],
-        [0, 0, 1, 1, 0],
-        [0, 0, 1, 0, 0],
-        [0, 1, 0, 0, 0],
-        [1, 1, 1, 1, 1]
-    ]),
-    np.array([
-        [0, 1, 1, 1, 0],
-        [1, 0, 0, 0, 1],
-        [0, 0, 0, 0, 1],
-        [0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [0, 1, 1, 1, 0]
-    ]),
-    np.array([
-        [0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 0],
-        [0, 1, 0, 1, 0],
-        [1, 0, 0, 1, 0],
-        [1, 1, 1, 1, 1],
-        [0, 0, 0, 1, 0],
-        [0, 0, 0, 1, 0]
-    ]),
-    np.array([
-        [1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [0, 1, 1, 1, 0]
-    ]),
-    np.array([
-        [0, 1, 1, 1, 0],
-        [1, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 0],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [0, 1, 1, 1, 0]
-    ]),
-    np.array([
-        [1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 1],
-        [0, 0, 0, 1, 0],
-        [0, 0, 1, 0, 0],
-        [0, 1, 0, 0, 0],
-        [0, 1, 0, 0, 0],
-        [0, 1, 0, 0, 0]
-    ]),
-    np.array([
-        [0, 1, 1, 1, 0],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [0, 1, 1, 1, 0],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [0, 1, 1, 1, 0]
-    ]),
-    np.array([
-        [0, 1, 1, 1, 0],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [0, 1, 1, 1, 1],
-        [0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [0, 1, 1, 1, 0]
-    ])
-]
-# конвретуємо двовимірний масив в вектор для кожного зображення
+import matplotlib.pyplot as plt
+import utils
 
-for image in initial_layer:
-  image.ravel()
+# Функція активації
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-# Вихідні дані
-reference = [
-    [0],
-    [1],
-    [0],
-    [1],
-    [0],
-    [1],
-    [0],
-    [1],
-    [0],
-    [1],
-  ]
+# Пряме поширення
+def forward_propagation(image, weight_input_to_hidden, bias_input_to_hidden, weight_hidden_to_output, bias_hidden_to_output):
+    hidden_raw = bias_input_to_hidden + weight_input_to_hidden @ image
+    hidden = sigmoid(hidden_raw)
+    output_raw = bias_hidden_to_output + weight_hidden_to_output @ hidden
+    output = sigmoid(output_raw)
+    return hidden, output
 
-Network = NeuroNetwork(initial_layer, reference)
-Network.learn(iterations=10_000)
-Network.show_predication()
-# Network.check_network()
-# Network.draw_graph()
+# Зворотнє поширення
+def backward_propagation(label, hidden, output, weight_hidden_to_output):
+    delta_output = output - label
+    delta_hidden = np.transpose(weight_hidden_to_output) @ delta_output * (hidden * (1 - hidden))
+    return delta_output, delta_hidden
+
+# Завантаження набору даних
+# зображення в форматі вектора / еталон
+images, labels = utils.load_dataset()
+
+# Ініціалізація ваг. генеруємо випадковими числами від -0.5 до 1.5
+weight_input_to_hidden = np.random.uniform(-0.5, 1.5, (20, 35))
+weight_hidden_to_output = np.random.uniform(-0.5, 1.5, (1, 20))
+# нейрони зміщення
+bias_input_to_hidden = np.zeros((20,1))
+bias_hidden_to_output = np.zeros((1,1))
+
+epochs = 250
+learning_rate = 0.1
+
+# Створення списку для зберігання точності на кожній епохі
+accuracy_list = []
+
+for epoch in range(epochs):
+    correct_predictions = 0
+    for image, label in zip(images, labels):
+        image = np.reshape(image, (-1,1))
+        label = np.reshape(label, (-1,1))
+
+        # Пряме поширення
+        hidden, output = forward_propagation(
+          image,
+          weight_input_to_hidden,
+          bias_input_to_hidden,
+          weight_hidden_to_output,
+          bias_hidden_to_output
+        )
+
+        # Зворотнє поширення помилки
+        delta_output, delta_hidden = backward_propagation(
+          label,
+          hidden,
+          output,
+          weight_hidden_to_output
+        )
+
+        # Оновлення ваг і зсувів
+        weight_hidden_to_output += -learning_rate * delta_output @ np.transpose(hidden)
+        bias_hidden_to_output += -learning_rate * delta_output
+        weight_input_to_hidden += -learning_rate * delta_hidden @ np.transpose(image)
+        bias_input_to_hidden += -learning_rate * delta_hidden
+
+        # Підрахунок правильних прогнозів
+        if np.round(output) == label:
+            correct_predictions += 1
+
+    # Розрахунок точності для поточної епохи та додавання до списку точності
+    accuracy = correct_predictions / len(images)
+    accuracy_list.append(accuracy)
+
+# Нові дані для тестування
+stop = False
+
+test_dataset = utils.load_test_dataset()
+while not stop:
+
+  index = int(input("Введіть номер:"))
+
+  test_image = np.reshape(test_dataset[index], (-1,1))
+
+  # Пряме поширення
+  hidden, output = forward_propagation(
+    test_image,
+    weight_input_to_hidden,
+    bias_input_to_hidden,
+    weight_hidden_to_output,
+    bias_hidden_to_output
+  )
+
+  # Прогнозування
+  prediction = 'Є коренем числа 2' if np.round(output[0][0]) == 1 else 'Не є коренем числа 2'
+
+  # Створення першого підграфа
+  plt.subplot(2, 1, 1) # (кількість рядків, кількість стовпців, індекс підграфа)
+  plt.imshow(test_image.reshape(7,5), cmap='gray')
+  plt.text(5.5, 1.5, f"Вірогідність: {output[0][0]}", fontsize=12)
+  plt.text(5.5, 2.5,f"Відповідь: {prediction}", fontsize=12)
+
+  # Створення другого підграфа
+  plt.subplot(2, 1, 2)
+  plt.plot(range(epochs), accuracy_list)
+  plt.title('Точність навчання за епохами')
+  plt.xlabel('Епохи')
+  plt.ylabel('Точність')
+
+  # Відображення обох графіків
+  plt.show()
