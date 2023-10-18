f978d5a4de4b0683e3ccff11873e5ae5f732862d
Illia <ilyusha.drozd.2016@gmail.com>
2023-10-18 15:36:25 +0300

    code refactoring

--- 2/main.py
+++ 2/main.py
@@ -6,7 +6,7 @@ import utils
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-# Пряме поширення
+# Пряме поширення для навчання
 def forward_propagation(image, weight_input_to_hidden, bias_input_to_hidden, weight_hidden_to_output, bias_hidden_to_output):
     hidden_raw = bias_input_to_hidden + weight_input_to_hidden @ image
     hidden = sigmoid(hidden_raw)
@@ -14,7 +14,7 @@ def forward_propagation(image, weight_input_to_hidden, bias_input_to_hidden, wei
     output = sigmoid(output_raw)
     return hidden, output
 
-# Зворотнє поширення
+# Зворотнє поширення для коригування вагів
 def backward_propagation(label, hidden, output, weight_hidden_to_output):
     delta_output = output - label
     delta_hidden = np.transpose(weight_hidden_to_output) @ delta_output * (hidden * (1 - hidden))
@@ -25,17 +25,26 @@ def backward_propagation(label, hidden, output, weight_hidden_to_output):
 images, labels = utils.load_dataset()
 
 # Ініціалізація ваг. генеруємо випадковими числами від -0.5 до 1.5
-weight_input_to_hidden = np.random.uniform(-0.5, 1.5, (20, 35))
-weight_hidden_to_output = np.random.uniform(-0.5, 1.5, (1, 20))
-# нейрони зміщення
-bias_input_to_hidden = np.zeros((20,1))
-bias_hidden_to_output = np.zeros((1,1))
 
-epochs = 250
-learning_rate = 0.1
+input_neurons = 35  # initial layer
+hidden_neurons = 35 # or 18
+output_neurons = 1  # result neuron
+
+epochs = 50 # or 250
+learning_rate = 0.3 # or 0.1
 
 # Створення списку для зберігання точності на кожній епохі
 accuracy_list = []
+
+neurons_input_to_hidden =  (hidden_neurons, input_neurons)
+neurons_hidden_to_output = (output_neurons, hidden_neurons)
+
+weight_input_to_hidden = np.random.uniform(-0.5, 1.5, neurons_input_to_hidden )
+weight_hidden_to_output = np.random.uniform(-0.5, 1.5, neurons_hidden_to_output )
+
+# нейрони зміщення
+bias_input_to_hidden = np.zeros((hidden_neurons,1))
+bias_hidden_to_output = np.zeros((output_neurons,output_neurons))
 
 for epoch in range(epochs):
     correct_predictions = 0
@@ -74,40 +83,43 @@ for epoch in range(epochs):
     accuracy = correct_predictions / len(images)
     accuracy_list.append(accuracy)
 
-# Нові дані для тестування
-stop = False
+def checkNN():
+  # Нові дані для тестування
+  stop = False
 
-test_dataset = utils.load_test_dataset()
-while not stop:
+  test_dataset = utils.load_test_dataset()
+  while not stop:
 
-  index = int(input("Введіть номер:"))
+    index = int(input("Введіть номер:"))
 
-  test_image = np.reshape(test_dataset[index], (-1,1))
+    test_image = np.reshape(test_dataset[index], (-1,1))
 
-  # Пряме поширення
-  hidden, output = forward_propagation(
-    test_image,
-    weight_input_to_hidden,
-    bias_input_to_hidden,
-    weight_hidden_to_output,
-    bias_hidden_to_output
-  )
+    # Пряме поширення
+    hidden, output = forward_propagation(
+      test_image,
+      weight_input_to_hidden,
+      bias_input_to_hidden,
+      weight_hidden_to_output,
+      bias_hidden_to_output
+    )
 
-  # Прогнозування
-  prediction = 'Є коренем числа 2' if np.round(output[0][0]) == 1 else 'Не є коренем числа 2'
+    # Прогнозування
+    prediction = 'Є коренем числа 2' if np.round(output[0][0]) == 1 else 'Не є коренем числа 2'
 
-  # Створення першого підграфа
-  plt.subplot(2, 1, 1) # (кількість рядків, кількість стовпців, індекс підграфа)
-  plt.imshow(test_image.reshape(7,5), cmap='gray')
-  plt.text(5.5, 1.5, f"Вірогідність: {output[0][0]}", fontsize=12)
-  plt.text(5.5, 2.5,f"Відповідь: {prediction}", fontsize=12)
+    # Створення першого підграфа
+    plt.subplot(2, 1, 1) # (кількість рядків, кількість стовпців, індекс підграфа)
+    plt.imshow(test_image.reshape(7,5), cmap='gray')
+    plt.text(5.5, 1.5, f"Вірогідність: {output[0][0]}", fontsize=12)
+    plt.text(5.5, 2.5,f"Відповідь: {prediction}", fontsize=12)
 
-  # Створення другого підграфа
-  plt.subplot(2, 1, 2)
-  plt.plot(range(epochs), accuracy_list)
-  plt.title('Точність навчання за епохами')
-  plt.xlabel('Епохи')
-  plt.ylabel('Точність')
+    # Створення другого підграфа
+    plt.subplot(2, 1, 2)
+    plt.plot(range(epochs), accuracy_list)
+    plt.title('Точність навчання за епохами')
+    plt.xlabel('Епохи')
+    plt.ylabel('Точність')
 
-  # Відображення обох графіків
-  plt.show()
+    # Відображення обох графіків
+    plt.show()
+
+checkNN()
